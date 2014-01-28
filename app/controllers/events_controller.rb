da79385dcd95a99bc301081291f018ffbd656a8a
adanot <adam.nottingham@gmail.com>
2014-01-28 14:52:11 +0000

    Basic event MVC

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,40 @@
+class EventsController < ApplicationController
+  before_action :signed_in_user, only: [:create, :edit, :destroy]
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+
+  end
+ 
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+	flash[:success] = "Event created!"
+	redirect_to root_url
+    else
+        render 'static_pages/home'
+    end
+  end
+
+  def show
+   @event = Event.find(params[:id])
+  end
+  
+  def index
+   @events = Event.all
+  end
+
+  def destroy
+    
+  end
+  
+  private
+
+    def event_params
+	params.require(:event).permit(:content)
+    end
+
+end
---
+++ app/models/event.rb
@@ -0,0 +1,5 @@
+class Event < ActiveRecord::Base
+	belongs_to :user
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :user_id, presence: true
+end
