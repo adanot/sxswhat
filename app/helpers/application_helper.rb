db6ee844938e38342dbf839ff9d8a9bb21e3c937
adanot <adam.nottingham@gmail.com>
2014-02-26 19:34:21 +0000

    badges added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,6 +50,6 @@ module ApplicationHelper
 
   def events_count(event_id)
 
-	@count ||= UserCalendar.where('event_id = ?', event_id).count
+	@count = UserCalendar.where('event_id = ?', event_id).count + 1
   end
 end
